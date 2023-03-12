894616c483a0d8cd78c7568ee083f355f21a023d
ukrainskykirill <ukrainsky.kirill@gmail.com>
2023-03-12 22:40:29 +0300

    test action commit

--- fpl_notes/fpl_notes/settings.py
+++ fpl_notes/fpl_notes/settings.py
@@ -16,9 +16,9 @@ DEBUG = env.bool("DEBUG", default=False)
 if not DEBUG:
     ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 else:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['http://*']
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
